2142d499176e37bb17ec1994062645a74e586ba4
Aakash <aakashbajaj5589@gmail.com>
2020-06-21 00:52:54 +0530

    footer 2020

--- frontend/src/app/shared/Footer.jsx
+++ frontend/src/app/shared/Footer.jsx
@@ -7,7 +7,7 @@ class Footer extends Component {
         <div className="container-fluid">
           <div className="d-sm-flex justify-content-center justify-content-sm-between">
             <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">
-              Copyright © 2019{" "}
+              Copyright © 2020{" "}
               <a
                 href="https://www.youngun.in/"
                 target="_blank"
