1b3b6f8293118a6c0c8de81218f48c11028d6490
Aakash <aakashbajaj5589@gmail.com>
2020-06-28 22:18:16 +0530

    invite user add fn working

--- backend/youngun/youngun/apps/usermanager/urls.py
+++ backend/youngun/youngun/apps/usermanager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProfileInfoRetriveAPIView
+from .views import ProfileInfoRetriveAPIView, InviteSubUsersAPIView
 
 app_name = 'profile'
 
 urlpatterns = [
     path("profile/", ProfileInfoRetriveAPIView.as_view()),
+    path("profile/inviteuser/", InviteSubUsersAPIView.as_view()),
 ]
--- backend/youngun/youngun/apps/usermanager/views.py
+++ backend/youngun/youngun/apps/usermanager/views.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from .serializers import ProfileSerializer
 from .renderers import ProfileJSONRenderer
 
+from youngun.apps.campaigns.models import Campaign
+from youngun.apps.authentication.models import User
+
 # Create your views here.
 
 
@@ -25,6 +28,27 @@ class InviteSubUsersAPIView(CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         if not request.user.profile.is_main_user:
-            return Response({"response": "Not Allowed"}, status.HTTP_401_UNAUTHORIZED)
+            return Response({"response": "Not Allowed. Not Main User"}, status.HTTP_401_UNAUTHORIZED)
 
         email = request.data["email"]
+
+        campaign_slug = request.data["campaign_slug"]
+        try:
+            camp_obj = Campaign.objects.get(slug=campaign_slug)
+        except Campaign.DoesNotExist:
+            return Response({"response": "Not Allowed. Invalid Campaign"}, status.HTTP_401_UNAUTHORIZED)
+
+        adder_prof_obj = request.user.profile
+
+        print(adder_prof_obj.campaigns.all())
+
+        if not camp_obj in adder_prof_obj.campaigns.all():
+            return Response({"response": "Not Allowed. Invalid Campaign", "payload": adder_prof_obj.campaigns.all()}, status.HTTP_401_UNAUTHORIZED)
+
+        invited_user, _ = User.objects.get_or_create(email=email)
+        invited_user.profile.added_by = adder_prof_obj
+        invited_user.profile.campaigns.add(camp_obj)
+
+        invited_user.profile.save()
+
+        return Response({"response": "Created"}, status.HTTP_201_CREATED)
