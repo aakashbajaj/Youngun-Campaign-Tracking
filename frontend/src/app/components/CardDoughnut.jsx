92ad4eb0eee83096f944b70e575b2e46690e5dd5
Aakash <aakashbajaj5589@gmail.com>
2020-06-28 21:52:45 +0530

    participating profiles on dash

--- frontend/src/app/components/CardDoughnut.jsx
+++ frontend/src/app/components/CardDoughnut.jsx
@@ -2,17 +2,6 @@ import React, { Component } from "react";
 import { Doughnut } from "react-chartjs-2";
 
 export default class CardDoughnut extends Component {
-  usersDoughnutChartData = {
-    datasets: [
-      {
-        data: [50, 50],
-        backgroundColor: ["#19d895", "#2196f3", "#dde4eb"],
-        borderColor: ["#19d895", "#2196f3", "#dde4eb"],
-      },
-    ],
-    labels: ["Approved", "Remaining"],
-  };
-
   getChartData(param1, param2) {
     return {
       datasets: [
@@ -62,11 +51,27 @@ export default class CardDoughnut extends Component {
               />
             </div>
             <div className="col-md-7">
+              {/* <h4 className="card-title font-weight-medium mb-2 d-none d-md-block">
+                Participating Profiles: {this.props.unique_content_pieces}
+                <p className="mb-0 font-weight-medium"></p>
+              </h4>
               <h4 className="card-title font-weight-medium mb-0 d-none d-md-block">
                 Unique Content Pieces: {this.props.unique_content_pieces}
-                {/* <p className="mb-0 font-weight-medium"></p> */}
-              </h4>
-              <div className="wrapper mt-4">
+                <p className="mb-0 font-weight-medium"></p>
+              </h4> */}
+              <div className="wrapper mt-2">
+                <h4 className="card-title font-weight-medium mb-2 ">
+                  Participating Profiles: {this.props.unique_content_pieces}
+                  {/* <p className="mb-0 font-weight-medium"></p> */}
+                </h4>
+                <h4 className="card-title font-weight-medium mb-2 ">
+                  Unique Content Pieces: {this.props.unique_content_pieces}
+                  {/* <p className="mb-0 font-weight-medium"></p> */}
+                </h4>
+
+                {/* <ProgressBar variant="primary" now={80} /> */}
+              </div>
+              <div className="wrapper mt-3">
                 <div className="d-flex justify-content-between mb-2">
                   <div className="d-flex align-items-center">
                     <p className="mb-0 font-weight-medium">
@@ -75,9 +80,6 @@ export default class CardDoughnut extends Component {
                     <small className="text-muted ml-2">Approved</small>
                   </div>
                 </div>
-                {/* <ProgressBar variant="primary" now={80} /> */}
-              </div>
-              <div className="wrapper mt-4">
                 <div className="d-flex justify-content-between mb-2">
                   <div className="d-flex align-items-center">
                     <p className="mb-0 font-weight-medium">
