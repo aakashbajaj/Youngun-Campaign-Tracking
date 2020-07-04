cebffac7cf152de1f56f0c070f41cc9ac12f0cf0
Aakash <aakashbajaj5589@gmail.com>
2020-07-04 15:30:06 +0530

    inline campaign filter

--- backend/youngun/youngun/apps/usermanager/admin.py
+++ backend/youngun/youngun/apps/usermanager/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from .models import Profile, Brand
+from .models import Profile, Brand, ClientProfile, StaffProfile
 # Register your models here.
 
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     pass
+
+@admin.register(StaffProfile)
+class StaffProfileAdmin(admin.ModelAdmin):
+    search_fields = ["campaigns"]
+    filter_horizontal = ['campaigns']
+
+
+@admin.register(ClientProfile)
+class ClientProfileAdmin(admin.ModelAdmin):
+    search_fields = ["campaigns"]
+    filter_horizontal = ['campaigns']
 
 
 @admin.register(Brand)
