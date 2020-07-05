def94e42181325bf9d580f1b25e2066ec3ee285d
Aakash <aakashbajaj5589@gmail.com>
2020-07-05 17:35:33 +0530

    save on top

--- backend/youngun/youngun/apps/content/admin.py
+++ backend/youngun/youngun/apps/content/admin.py
@@ -44,7 +44,7 @@ class PostAdmin(admin.ModelAdmin):
 @admin.register(InstagramPost)
 class InstagramPostAdmin(admin.ModelAdmin):
     exclude = ('platform', 'post_type')
-    list_display = ('url', 'link_to_camp', 'date', 'likes', 'comments',
+    list_display = ('url', 'campaign', 'link_to_camp', 'date', 'likes', 'comments',
                     'post_shares', 'post_saves', 'post_reach')
 
     readonly_fields = ('date', 'link_to_camp')
@@ -58,6 +58,8 @@ class InstagramPostAdmin(admin.ModelAdmin):
     list_filter = [
         ('campaign__name', custom_titled_filter("Campaign")),
     ]
+
+    save_on_top = True
 
     def link_to_camp(self, obj):
         link = reverse("admin:campaigns_campaign_change",
@@ -82,23 +84,81 @@ class InstagramPostAdmin(admin.ModelAdmin):
 @admin.register(FacebookPost)
 class FacebookPostAdmin(admin.ModelAdmin):
     exclude = ('platform', 'embed_code')
-    list_display = ('url', 'campaign', 'date', 'likes', 'comments',
+    list_display = ('url', 'campaign', 'link_to_camp', 'date', 'likes', 'comments',
                     'post_shares', 'post_saves', 'post_reach')
+
+    readonly_fields = ('date', 'link_to_camp')
+    fields = ('url', 'campaign', 'link_to_camp', 'date', 'likes', 'comments',
+              'post_shares', 'post_saves', 'post_reach', 'embed_code', 'visibility', 'alt_google_photo_url')
+    # list_display_links = ('campaign', )
+
+    # add_fields = ('url', 'campaign', 'date', 'likes', 'comments',
+    #               'post_shares', 'post_saves', 'post_reach', 'embed_code', 'visibility', 'alt_google_photo_url')
 
     list_filter = [
         ('campaign__name', custom_titled_filter("Campaign")),
     ]
+
+    save_on_top = True
+
+    def link_to_camp(self, obj):
+        link = reverse("admin:campaigns_campaign_change",
+                       args=[obj.campaign.id])
+
+        link_live = reverse("admin:campaigns_livecampaign_change",
+                            args=[obj.campaign.id])
+
+        link_report = reverse("admin:campaigns_campaignreport_change",
+                              args=[obj.campaign.id])
+        return format_html('<a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/>', link, "Campaign Admin", link_live, "Live Details", link_report, "Report Data")
+
+    link_to_camp.short_description = "Campaign URLs"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "campaign":
+            kwargs["queryset"] = Campaign.objects.filter(
+                staff_profiles=request.user.usermanager_staffprofile)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 @admin.register(TwitterPost)
 class TwitterPostAdmin(admin.ModelAdmin):
     exclude = ('platform', 'post_type')
-    list_display = ('url', 'campaign', 'date', 'likes', 'comments',
+    list_display = ('url', 'campaign', 'link_to_camp', 'date', 'likes', 'comments',
                     'post_shares', 'post_saves', 'post_reach')
+
+    readonly_fields = ('date', 'link_to_camp')
+    fields = ('url', 'campaign', 'link_to_camp', 'date', 'likes', 'comments',
+              'post_shares', 'post_saves', 'post_reach', 'embed_code', 'visibility', 'alt_google_photo_url')
+    # list_display_links = ('campaign', )
+
+    # add_fields = ('url', 'campaign', 'date', 'likes', 'comments',
+    #               'post_shares', 'post_saves', 'post_reach', 'embed_code', 'visibility', 'alt_google_photo_url')
 
     list_filter = [
         ('campaign__name', custom_titled_filter("Campaign")),
     ]
+
+    save_on_top = True
+
+    def link_to_camp(self, obj):
+        link = reverse("admin:campaigns_campaign_change",
+                       args=[obj.campaign.id])
+
+        link_live = reverse("admin:campaigns_livecampaign_change",
+                            args=[obj.campaign.id])
+
+        link_report = reverse("admin:campaigns_campaignreport_change",
+                              args=[obj.campaign.id])
+        return format_html('<a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/>', link, "Campaign Admin", link_live, "Live Details", link_report, "Report Data")
+
+    link_to_camp.short_description = "Campaign URLs"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "campaign":
+            kwargs["queryset"] = Campaign.objects.filter(
+                staff_profiles=request.user.usermanager_staffprofile)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 @admin.register(InstagramStory)
@@ -110,6 +170,27 @@ class InstagramStoryAdmin(admin.ModelAdmin):
         ('campaign__name', custom_titled_filter("Campaign")),
     ]
 
+    save_on_top = True
+
+    def link_to_camp(self, obj):
+        link = reverse("admin:campaigns_campaign_change",
+                       args=[obj.campaign.id])
+
+        link_live = reverse("admin:campaigns_livecampaign_change",
+                            args=[obj.campaign.id])
+
+        link_report = reverse("admin:campaigns_campaignreport_change",
+                              args=[obj.campaign.id])
+        return format_html('<a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/>', link, "Campaign Admin", link_live, "Live Details", link_report, "Report Data")
+
+    link_to_camp.short_description = "Campaign URLs"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "campaign":
+            kwargs["queryset"] = Campaign.objects.filter(
+                staff_profiles=request.user.usermanager_staffprofile)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 @admin.register(FacebookStory)
 class FacebookStoryAdmin(admin.ModelAdmin):
@@ -120,6 +201,27 @@ class FacebookStoryAdmin(admin.ModelAdmin):
         ('campaign__name', custom_titled_filter("Campaign")),
     ]
 
+    save_on_top = True
+
+    def link_to_camp(self, obj):
+        link = reverse("admin:campaigns_campaign_change",
+                       args=[obj.campaign.id])
+
+        link_live = reverse("admin:campaigns_livecampaign_change",
+                            args=[obj.campaign.id])
+
+        link_report = reverse("admin:campaigns_campaignreport_change",
+                              args=[obj.campaign.id])
+        return format_html('<a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/>', link, "Campaign Admin", link_live, "Live Details", link_report, "Report Data")
+
+    link_to_camp.short_description = "Campaign URLs"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "campaign":
+            kwargs["queryset"] = Campaign.objects.filter(
+                staff_profiles=request.user.usermanager_staffprofile)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 @admin.register(TwitterStory)
 class TwitterStoryAdmin(admin.ModelAdmin):
@@ -129,3 +231,24 @@ class TwitterStoryAdmin(admin.ModelAdmin):
     list_filter = [
         ('campaign__name', custom_titled_filter("Campaign")),
     ]
+
+    save_on_top = True
+
+    def link_to_camp(self, obj):
+        link = reverse("admin:campaigns_campaign_change",
+                       args=[obj.campaign.id])
+
+        link_live = reverse("admin:campaigns_livecampaign_change",
+                            args=[obj.campaign.id])
+
+        link_report = reverse("admin:campaigns_campaignreport_change",
+                              args=[obj.campaign.id])
+        return format_html('<a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/><br/><a href="{}">{}</a><br/>', link, "Campaign Admin", link_live, "Live Details", link_report, "Report Data")
+
+    link_to_camp.short_description = "Campaign URLs"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "campaign":
+            kwargs["queryset"] = Campaign.objects.filter(
+                staff_profiles=request.user.usermanager_staffprofile)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
