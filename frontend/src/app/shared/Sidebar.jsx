4a0da3302a615a1de1750f48987d3a987cc306df
Aakash <aakashbajaj5589@gmail.com>
2020-07-26 13:22:21 +0530

    sidebar entries

--- frontend/src/app/shared/Sidebar.jsx
+++ frontend/src/app/shared/Sidebar.jsx
@@ -63,36 +63,15 @@ class Sidebar extends Component {
               <span className="menu-title">Dashboard</span>
             </Link>
           </li>
-          <li
-            className={
-              this.isPathActive("/instafeed") ? "nav-item active" : "nav-item"
-            }
-          >
-            <Link className="nav-link" to="/instafeed">
-              <i className="mdi mdi-instagram menu-icon"></i>
-              <span className="menu-title">Instagram Feed</span>
-            </Link>
-          </li>
 
           <li
             className={
-              this.isPathActive("/fbfeed") ? "nav-item active" : "nav-item"
+              this.isPathActive("/postsfeed") ? "nav-item active" : "nav-item"
             }
           >
-            <Link className="nav-link" to="/fbfeed">
-              <i className="mdi mdi-facebook menu-icon"></i>
-              <span className="menu-title">Facebook Feed</span>
-            </Link>
-          </li>
-
-          <li
-            className={
-              this.isPathActive("/twitterfeed") ? "nav-item active" : "nav-item"
-            }
-          >
-            <Link className="nav-link" to="/twitterfeed">
-              <i className="mdi mdi-twitter menu-icon"></i>
-              <span className="menu-title">Twitter Feed</span>
+            <Link className="nav-link" to="/postsfeed">
+              <i className="mdi mdi-fullscreen menu-icon"></i>
+              <span className="menu-title">Posts Feed</span>
             </Link>
           </li>
 
