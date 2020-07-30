40fa1fb3c2d5837b2601f5d9ed5fa548228d3317
Aakash <aakashbajaj5589@gmail.com>
2020-07-30 14:49:55 +0530

    backend docker setup

---
+++ backend/youngun/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+rm *.sqlite3
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py collectstatic --noinput
+
+nohup python manage.py qcluster &
+python manage.py runserver 0.0.0.0:8000
--- backend/youngun/youngun/settings.py
+++ backend/youngun/youngun/settings.py
@@ -170,6 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = get_secret("SENDER_EMAIL")
