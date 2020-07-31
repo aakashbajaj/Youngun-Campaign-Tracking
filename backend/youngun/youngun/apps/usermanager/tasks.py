3474abf33b28070c6a228f074ea5af6da2b928ef
Aakash <aakashbajaj5589@gmail.com>
2020-08-01 00:59:26 +0530

    invite email change

--- backend/youngun/youngun/apps/usermanager/tasks.py
+++ backend/youngun/youngun/apps/usermanager/tasks.py
@@ -7,8 +7,8 @@ from django_q.models import Schedule
 
 
 def send_invite_mail(email, campaign_name, user_name):
-    msg = "Hi!\nYou've been invited to view {0} by {1}.\nPlease login using your Email ID on https://tracking.youngun.in".format(
-        campaign_name, user_name)
-    subj = "{0} Tracking Invite".format(campaign_name)
+    msg = "Hi!\nYou've been invited to view a meme campaign by {0}.\nPlease login using your Email ID on https://tracking.youngun.in".format(
+        user_name)
+    subj = "{0} Meme Campaign Invite".format(campaign_name)
     async_task('django.core.mail.send_mail', subj,
                msg, "support@youngun.in", [email])
