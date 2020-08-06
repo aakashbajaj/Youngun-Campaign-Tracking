c93ced1f07e4edd586df4ae11f54b0e7ba6ef82f
Aakash <aakashbajaj5589@gmail.com>
2020-08-06 10:27:09 +0530

    twitter coll in UI

--- frontend/src/app/pages/TwitterFeed.jsx
+++ frontend/src/app/pages/TwitterFeed.jsx
@@ -19,6 +19,7 @@ export default class TwitterFeed extends Component {
 
   render() {
     var twitterposts = [];
+    var tw_coll_url = "";
     if (
       this.context.currentCampaignInView !== null &&
       this.context.liveCampaignFeed[this.context.currentCampaignInView] !==
@@ -29,15 +30,17 @@ export default class TwitterFeed extends Component {
       twitterposts = this.context.liveCampaignFeed[
         this.context.currentCampaignInView
       ].twitter;
+      tw_coll_url = this.context.liveCampaignFeed[
+        this.context.currentCampaignInView
+      ].twitter_collection_url;
     }
 
-    if (!twitterposts) {
+    if (!tw_coll_url) {
       return <Spinner />;
     }
     return (
       <div>
-        <div className="row">
-          {/* <div className="card-columns"> */}
+        {/* <div className="row">
           {twitterposts.map((post, idx) => {
             if (post.embed_code !== "") {
               return (
@@ -46,10 +49,6 @@ export default class TwitterFeed extends Component {
                     className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
                     dangerouslySetInnerHTML={this.createMarkup(post.embed_code)}
                   />
-                  {/* <div key={idx} className="card">
-                  <div
-                    dangerouslySetInnerHTML={this.createMarkup(post.embed_code)}
-                  /> */}
                 </div>
               );
             } else if (
@@ -74,6 +73,19 @@ export default class TwitterFeed extends Component {
             }
             return null;
           })}
+        </div> */}
+        <div
+          style={{
+            width: "50%",
+            margin: "0 auto",
+          }}
+        >
+          <a class="twitter-timeline" data-width="550" href={tw_coll_url}></a>{" "}
+          <script
+            async
+            src="https://platform.twitter.com/widgets.js"
+            charset="utf-8"
+          ></script>
         </div>
       </div>
     );
