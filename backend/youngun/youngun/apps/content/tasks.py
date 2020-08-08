686471b0447ea06395bed537c09e0f8ac1316d7a
Aakash <aakashbajaj5589@gmail.com>
2020-08-09 02:53:54 +0530

    fetch username from oembed data

--- backend/youngun/youngun/apps/content/tasks.py
+++ backend/youngun/youngun/apps/content/tasks.py
@@ -1,13 +1,22 @@
-from youngun.apps.content.models import InstagramPost, TwitterPost, FacebookPost
+import requests
+from youngun.apps.content.models import InstagramPost, TwitterPost, FacebookPost, PostVisibility
 
 
 def extract_username_from_posts():
     for post in InstagramPost.objects.all():
-        if post.embed_code.startswith("<blockquote"):
-            start_idx = post.embed_code.index("(@")
-            if start_idx > 0:
-                end_idx = post.embed_code[start_idx:].index(")")
-                post.post_username = post.embed_code[start_idx +
-                                                     2:end_idx+start_idx]
+        post_url = post.url
+        fetch_url = "https://api.instagram.com/oembed"
+        params = {
+            'url': post_url
+        }
 
-                post.save()
+        res = requests.get(fetch_url, params=params)
+        if res.status_code == 404:
+            post.visibility = PostVisibility.PRIVATE
+
+        elif res.status_code == 200:
+            # fetched_embed = res.json()["html"]
+            # post.embed_code = fetched_embed
+            post.post_username = res.json()["author_name"]
+
+        post.save()
