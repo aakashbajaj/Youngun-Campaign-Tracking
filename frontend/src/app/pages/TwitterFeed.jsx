8721c30eb3625a126a9e6a90054a45e4d6604a67
Aakash <aakashbajaj5589@gmail.com>
2020-08-10 02:05:11 +0530

    infinite scroll added to twitter

--- frontend/src/app/pages/TwitterFeed.jsx
+++ frontend/src/app/pages/TwitterFeed.jsx
@@ -2,15 +2,50 @@ import React, { Component } from "react";
 import CampaignContext from "../data/CampaignContext";
 import Spinner from "../shared/Spinner";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export default class TwitterFeed extends Component {
   static contextType = CampaignContext;
 
   createMarkup(embed_code) {
     return { __html: embed_code };
   }
-  componentDidMount() {
-    console.log("in FB CDM");
+
+  state = {
+    allTwitterPosts: [],
+    displayTwitterPosts: [],
+    hasMore: true,
+  };
+
+  showMorePosts = () => {
+    var currPostCount = this.state.displayTwitterPosts.length;
+
+    if (
+      currPostCount >=
+      this.context.liveCampaignFeed[this.context.currentCampaignInView].twitter
+        .length
+    ) {
+      this.setState({ hasMore: false });
+    }
+    this.setState({
+      displayTwitterPosts: this.state.displayTwitterPosts.concat(
+        this.context.liveCampaignFeed[
+          this.context.currentCampaignInView
+        ].twitter.slice(currPostCount, currPostCount + 9)
+      ),
+    });
     window.twttr.widgets.load();
+  };
+
+  componentDidMount() {
+    console.log("in TW CDM");
+    window.twttr.widgets.load();
+
+    this.setState({
+      displayTwitterPosts: this.context.liveCampaignFeed[
+        this.context.currentCampaignInView
+      ].twitter.slice(0, 9),
+    });
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -18,83 +53,78 @@ export default class TwitterFeed extends Component {
   }
 
   render() {
-    var twitterposts = [];
-    // var tw_coll_url = "";
-    if (
-      this.context.currentCampaignInView !== null &&
-      this.context.liveCampaignFeed[this.context.currentCampaignInView] !==
-        null &&
-      this.context.liveCampaignFeed[this.context.currentCampaignInView]
-        .twitter !== null
-    ) {
-      twitterposts = this.context.liveCampaignFeed[
-        this.context.currentCampaignInView
-      ].twitter;
-      // tw_coll_url = this.context.liveCampaignFeed[
-      //   this.context.currentCampaignInView
-      // ].twitter_collection_url;
-    }
+    // var twitterposts = [];
+    // if (
+    //   this.context.currentCampaignInView !== null &&
+    //   this.context.liveCampaignFeed[this.context.currentCampaignInView] !==
+    //     null &&
+    //   this.context.liveCampaignFeed[this.context.currentCampaignInView]
+    //     .twitter !== null
+    // ) {
+    //   twitterposts = this.context.liveCampaignFeed[
+    //     this.context.currentCampaignInView
+    //   ].twitter;
+    // }
 
-    // if (!tw_coll_url) {
+    var postsOnPage = this.state.displayTwitterPosts.map((post, idx) => {
+      if (post.embed_code !== "") {
+        return (
+          <div key={idx}>
+            <div
+              className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
+              dangerouslySetInnerHTML={this.createMarkup(post.embed_code)}
+            />
+          </div>
+        );
+      } else if (
+        post.alt_google_photo_url !== "" &&
+        post.alt_google_photo_url !== null
+      ) {
+        return (
+          <div
+            key={idx}
+            className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
+          >
+            <a href={post.url} target="_blank" rel="noopener noreferrer">
+              <img
+                key={post.alt_google_photo_url}
+                src={post.alt_google_photo_url}
+                alt={"Post"}
+                loader={<Spinner />}
+              />
+            </a>
+          </div>
+        );
+      }
+      return null;
+    });
+
+    // if (!twitterposts || twitterposts.length === 0) {
     //   return <Spinner />;
     // }
-    if (!twitterposts) {
+
+    if (
+      !this.state.displayTwitterPosts ||
+      this.state.displayTwitterPosts.length === 0
+    ) {
       return <Spinner />;
     }
+
     return (
       <div>
-        <div className="row">
-          {twitterposts.map((post, idx) => {
-            if (post.embed_code !== "") {
-              return (
-                <div key={idx}>
-                  <div
-                    className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
-                    dangerouslySetInnerHTML={this.createMarkup(post.embed_code)}
-                  />
-                </div>
-              );
-            } else if (
-              post.alt_google_photo_url !== "" &&
-              post.alt_google_photo_url !== null
-            ) {
-              return (
-                <div
-                  key={idx}
-                  className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
-                >
-                  <a href={post.url} target="_blank" rel="noopener noreferrer">
-                    <img
-                      key={post.alt_google_photo_url}
-                      src={post.alt_google_photo_url}
-                      alt={"Post"}
-                      loader={<Spinner />}
-                    />
-                  </a>
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
-        {/* <div className="row">
-          <div className="col-xl-3 col-lg-3 col-md-2 hidden-sm"></div>
-          <div
-            className="col-xl-6 col-lg-6 col-md-6 col-sm-12"
-            // style={{
-            //   width: "50%",
-            //   margin: "0 auto",
-            // }}
-          >
-            <a class="twitter-timeline" data-width="550" href={tw_coll_url}></a>{" "}
-            <script
-              async
-              src="https://platform.twitter.com/widgets.js"
-              charset="utf-8"
-            ></script>
-          </div>
-          <div className="col-xl-3 col-lg-3 col-md-2 hidden-sm"></div>
-        </div> */}
+        <InfiniteScroll
+          dataLength={this.state.displayTwitterPosts.length}
+          next={this.showMorePosts}
+          hasMore={this.state.hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>You have seen it all</b>
+            </p>
+          }
+        >
+          <div className="row">{postsOnPage}</div>
+        </InfiniteScroll>
       </div>
     );
   }
