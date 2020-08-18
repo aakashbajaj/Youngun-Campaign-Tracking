a72028c9d3a33d1a6c0601cfc223965e46306b86
Aakash <aakashbajaj5589@gmail.com>
2020-08-19 00:02:16 +0530

    login count working

--- backend/youngun/youngun/apps/core/admin.py
+++ backend/youngun/youngun/apps/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import MasterLogger
 # Register your models here.
+
+
+@admin.register(MasterLogger)
+class MasterLoggerAdmin(admin.ModelAdmin):
+    list_display = ['email', 'login_cnt', 'last_login']
+    readonly_fields = ('user', 'email', 'login_cnt',
+                       'last_login', 'history_log')
