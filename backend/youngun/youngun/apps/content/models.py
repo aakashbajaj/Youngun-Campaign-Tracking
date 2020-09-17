67adca74ad973358ece2b8e7af2df8cbeddac868
Aakash Bajaj <aakashbajaj5589@gmail.com>
2020-09-17 19:19:37 +0530

    google photo url length to 8000

--- backend/youngun/youngun/apps/content/models.py
+++ backend/youngun/youngun/apps/content/models.py
@@ -35,7 +35,7 @@ class Post(models.Model):
 
     embed_code = models.TextField(_("embed code"), null=True, blank=True)
     alt_google_photo_url = models.URLField(
-        _("Alternate Google Photo URL"), max_length=4000, null=True, blank=True)
+        _("Alternate Google Photo URL"), max_length=8000, null=True, blank=True)
     visibility = models.CharField(
         _("post visibility"), choices=PostVisibility.choices, max_length=50, default='public')
 
