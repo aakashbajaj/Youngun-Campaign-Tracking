319a441bc8bc27b7489d090a11239e95c4704647
Aakash Bajaj <aakashbajaj5589@gmail.com>
2021-01-16 21:02:28 +0530

    New twitter feed

--- frontend/src/app/pages/Feeds/TwitterFeed.jsx
+++ frontend/src/app/pages/Feeds/TwitterFeed.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import CampaignContext from "../../data/CampaignContext";
 import Spinner from "../../shared/Spinner";
 
-import InfiniteScroll from "react-infinite-scroll-component";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import TwitterCard from "./TwitterCard";
+import Tweet from "../../components/Tweet";
 
 export default class TwitterFeed extends Component {
   static contextType = CampaignContext;
@@ -17,29 +21,29 @@ export default class TwitterFeed extends Component {
     hasMore: true,
   };
 
-  showMorePosts = () => {
-    var currPostCount = this.state.displayTwitterPosts.length;
+  // showMorePosts = () => {
+  //   var currPostCount = this.state.displayTwitterPosts.length;
 
-    if (
-      currPostCount >=
-      this.context.liveCampaignFeed[this.context.currentCampaignInView].twitter
-        .length
-    ) {
-      this.setState({ hasMore: false });
-    }
-    this.setState({
-      displayTwitterPosts: this.state.displayTwitterPosts.concat(
-        this.context.liveCampaignFeed[
-          this.context.currentCampaignInView
-        ].twitter.slice(currPostCount, currPostCount + 9)
-      ),
-    });
-    window.twttr.widgets.load();
-  };
+  //   if (
+  //     currPostCount >=
+  //     this.context.liveCampaignFeed[this.context.currentCampaignInView].twitter
+  //       .length
+  //   ) {
+  //     this.setState({ hasMore: false });
+  //   }
+  //   this.setState({
+  //     displayTwitterPosts: this.state.displayTwitterPosts.concat(
+  //       this.context.liveCampaignFeed[
+  //         this.context.currentCampaignInView
+  //       ].twitter.slice(currPostCount, currPostCount + 9)
+  //     ),
+  //   });
+  //   window.twttr.widgets.load();
+  // };
 
   componentDidMount() {
     console.log("in TW CDM");
-    window.twttr.widgets.load();
+    // window.twttr.widgets.load();
 
     this.setState({
       displayTwitterPosts: this.context.liveCampaignFeed[
@@ -49,30 +53,71 @@ export default class TwitterFeed extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    window.twttr.widgets.load();
+    // window.twttr.widgets.load();
   }
 
   render() {
-    // var twitterposts = [];
-    // if (
-    //   this.context.currentCampaignInView !== null &&
-    //   this.context.liveCampaignFeed[this.context.currentCampaignInView] !==
-    //     null &&
-    //   this.context.liveCampaignFeed[this.context.currentCampaignInView]
-    //     .twitter !== null
-    // ) {
-    //   twitterposts = this.context.liveCampaignFeed[
-    //     this.context.currentCampaignInView
-    //   ].twitter;
-    // }
+    var twitterposts = [];
+    if (
+      this.context.currentCampaignInView !== null &&
+      this.context.liveCampaignFeed[this.context.currentCampaignInView] !==
+        null &&
+      this.context.liveCampaignFeed[this.context.currentCampaignInView]
+        .twitter !== null
+    ) {
+      twitterposts = this.context.liveCampaignFeed[
+        this.context.currentCampaignInView
+      ].twitter;
+    }
 
-    var postsOnPage = this.state.displayTwitterPosts.map((post, idx) => {
+    // var postsOnPage = this.state.displayTwitterPosts.map((post, idx) => {
+    //   if (post.embed_code !== "") {
+    //     return (
+    //       <div key={idx}>
+    //         <div
+    //           className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
+    //           dangerouslySetInnerHTML={this.createMarkup(post.embed_code)}
+    //         />
+    //       </div>
+    //     );
+    //   } else if (
+    //     post.alt_google_photo_url !== "" &&
+    //     post.alt_google_photo_url !== null
+    //   ) {
+    //     return (
+    //       <div
+    //         key={idx}
+    //         className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
+    //       >
+    //         <a href={post.url} target="_blank" rel="noopener noreferrer">
+    //           <img
+    //             key={post.alt_google_photo_url}
+    //             src={post.alt_google_photo_url}
+    //             alt={"Post"}
+    //             loader={<Spinner />}
+    //           />
+    //         </a>
+    //       </div>
+    //     );
+    //   }
+    //   return null;
+    // });
+
+    var postsOnPage = twitterposts.map((post, idx) => {
       if (post.embed_code !== "") {
         return (
           <div key={idx}>
-            <div
-              className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
-              dangerouslySetInnerHTML={this.createMarkup(post.embed_code)}
+            <Tweet
+              post_url={post.url}
+              profile_img_url={post.prof_img_url}
+              account_name={post.account_name}
+              account_username={post.post_username}
+              caption={post.caption}
+              comment_cnt={post.comments}
+              retweet_cnt={post.post_shares}
+              like_cnt={post.likes}
+              upload_date={post.upload_date}
+              media_objs={post.media_objs}
             />
           </div>
         );
@@ -81,10 +126,7 @@ export default class TwitterFeed extends Component {
         post.alt_google_photo_url !== null
       ) {
         return (
-          <div
-            key={idx}
-            className="col-xl-4 col-lg-4 col-md-6 col-sm-12 grid-margin stretch-card"
-          >
+          <div key={idx}>
             <a href={post.url} target="_blank" rel="noopener noreferrer">
               <img
                 key={post.alt_google_photo_url}
@@ -99,9 +141,9 @@ export default class TwitterFeed extends Component {
       return null;
     });
 
-    // if (!twitterposts || twitterposts.length === 0) {
-    //   return <Spinner />;
-    // }
+    if (!twitterposts || twitterposts.length === 0) {
+      return <Spinner />;
+    }
 
     if (
       !this.state.displayTwitterPosts ||
@@ -111,21 +153,29 @@ export default class TwitterFeed extends Component {
     }
 
     return (
-      <div>
-        <InfiniteScroll
-          dataLength={this.state.displayTwitterPosts.length}
-          next={this.showMorePosts}
-          hasMore={this.state.hasMore}
-          loader={<h4>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: "center" }}>
-              <b>You have seen it all</b>
-            </p>
-          }
-        >
-          <div className="row">{postsOnPage}</div>
-        </InfiniteScroll>
-      </div>
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200: 4 }}
+      >
+        <Masonry gutter={20}>{postsOnPage}</Masonry>
+      </ResponsiveMasonry>
     );
+
+    // return (
+    //   <div>
+    //     <InfiniteScroll
+    //       dataLength={this.state.displayTwitterPosts.length}
+    //       next={this.showMorePosts}
+    //       hasMore={this.state.hasMore}
+    //       loader={<h4>Loading...</h4>}
+    //       endMessage={
+    //         <p style={{ textAlign: "center" }}>
+    //           <b>You have seen it all</b>
+    //         </p>
+    //       }
+    //     >
+    //       <div className="row">{postsOnPage}</div>
+    //     </InfiniteScroll>
+    //   </div>
+    // );
   }
 }
