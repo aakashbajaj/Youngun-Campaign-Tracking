c2c0f77f6990a9d4773a7105734d33afe4597bc2
Aakash Bajaj <aakashbajaj5589@gmail.com>
2021-02-21 17:02:05 +0530

    adjust total post cnt display for sm

--- frontend/src/app/pages/Reporting.jsx
+++ frontend/src/app/pages/Reporting.jsx
@@ -36,11 +36,11 @@ export default class Reporting extends Component {
               <div className="card-body py-4">
                 <div className="d-flex flex-row justify-content-center align-items">
                   <i
-                    className={`mdi mt-5 ml-3 mdi-account-box-multiple text-warning icon-lg`}
+                    className={`mdi mt-lg-5 mt-sm-2 ml-3 mdi-account-box-multiple text-warning icon-lg`}
                   ></i>
                   <div className="ml-4">
                     {/* <p className="text-muted card-text mt-2 mb-1">₹ 0.34</p> */}
-                    <h6 className={`font-weight-semibold mt-5 mb-1`}>
+                    <h6 className={`font-weight-semibold mt-lg-5 mt-sm-2 mb-1`}>
                       Number of Posts
                     </h6>
                     <h6 className={`font-weight-bold mt-2`}>
@@ -208,7 +208,9 @@ export default class Reporting extends Component {
             </div>
           </div> */}
 
-          {this.props.showCosts ? (
+          {this.props.showCosts &&
+          campReportData.cost_per_reach !== "" &&
+          campReportData.cost_per_engagement !== "" ? (
             <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin stretch-card">
               <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 grid-margin stretch-card">
                 <div className="card">
