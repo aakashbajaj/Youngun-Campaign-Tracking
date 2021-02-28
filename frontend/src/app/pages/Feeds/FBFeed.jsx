ad76310ed47d2ee097f53510526c6840a3d54d56
Aakash Bajaj <aakashbajaj5589@gmail.com>
2021-03-01 01:44:57 +0530

    fb feed

--- frontend/src/app/pages/Feeds/FBFeed.jsx
+++ frontend/src/app/pages/Feeds/FBFeed.jsx
@@ -49,13 +49,13 @@ export default class FBFeed extends Component {
                   <div
                     className="fb-post"
                     data-href={`${post.url}`}
-                    data-width="400"
+                    data-width="300"
                   />
                 ) : (
                   <div
                     class="fb-video"
                     data-href={`${post.url}`}
-                    data-width="400"
+                    data-width="300"
                     data-allowfullscreen="true"
                     data-autoplay="false"
                     data-show-captions="true"
